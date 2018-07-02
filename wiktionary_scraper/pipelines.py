8c5fbfe54500f2b2581b3d9c4109e7e73a700f13
Declan Nishiyama <dnishiyama@gmail.com>
2018-07-02 05:51:08 +0000

    Added error handling for 404

--- wiktionary_scraper/pipelines.py
+++ wiktionary_scraper/pipelines.py
@@ -9,97 +9,96 @@ import mysql.connector, os
 
 class WiktionaryScraperPipeline(object):
 
-    collection_name = 'scrapy_items'
+	collection_name = 'scrapy_items'
 
-    def __init__(self):
-        self.user = os.environ['MYSQL_DB_USER']
-        self.password = os.environ['MYSQL_DB_PASSWORD']
-        self.host = '127.0.0.1'
-        self.database = 'etymology_explorer_staging'
+	def __init__(self):
+		self.user = os.environ['MYSQL_DB_USER']
+		self.password = os.environ['MYSQL_DB_PASSWORD']
+		self.host = '127.0.0.1'
+		self.database = 'etymology_explorer_staging'
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        return cls()
+	@classmethod
+	def from_crawler(cls, crawler):
+		return cls()
 
-    def open_spider(self, spider):
-        self.conn = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
-        self.cursor = self.conn.cursor()
-        self.cursor.execute('SET NAMES utf8mb4;') #To avoid unicode issues
+	def open_spider(self, spider):
+		self.conn = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+		self.cursor = self.conn.cursor()
+		self.cursor.execute('SET NAMES utf8mb4;') #To avoid unicode issues
 
-    def close_spider(self, spider):
-        self.conn.commit()
-        self.conn.close()
+	def close_spider(self, spider):
+		self.conn.commit()
+		self.conn.close()
 
-    def process_item(self, item, spider):
-        
-        all_pos = ['adfix', 'adjective', 'adnoun', 'adverb', 'article', 'auxiliary verb', 'cardinal number', 'collective numeral',
-           'conjunction', 'coverb', 'demonstrative determiner', 'demonstrative pronoun', 'determinative', 'determiner',
-           'gerund', 'indefinite pronoun', 'infinitive', 'interjection', 'interrogative pronoun', 'intransitive verb',
-           'noun', 'number', 'numeral', 'ordinal', 'ordinal number', 'part of speech', 'participle', 'particle',
-           'personal pronoun', 'phrasal preposition', 'possessive adjective', 'possessive determiner', 'possessive pronoun',
-           'postposition', 'preposition', 'preverb', 'pronoun', 'quasi-adjective', 'reciprocal pronoun', 'reflexive pronoun',
-           'relative pronoun', 'speech disfluency', 'substantive', 'transitive', 'transitive verb', 'verb', 'verbal noun']
-        all_pos += ['infix', 'suffix', 'prefix', 'root']; # Needed for reconstruction
-        
-        def getNewKey(column, table):
-            try:
-                self.cursor.execute(f'SELECT max({column}) FROM {table}')
-            except Exception as e:
-                self.cursor.fetchall(); #Fetch to prevent this error from cascading
-                raise (e) # Raise so that no errors are introduced to the MYSQL Database
-            max_entry_id = self.cursor.fetchone()[0]; 
-            new_entry_id = max_entry_id + 1 if max_entry_id is not None else 0
-            return new_entry_id
-        
-        def insert(table, **kwargs):
-            columns = [str(k) for k,v in kwargs.items()]
-            col_text = '('+', '.join(columns)+')'
-            
-            values = [str(v) if type(v) != str else repr(v) for k,v in kwargs.items()]
-            val_text = '('+', '.join(values)+')'
-            
-            self.cursor.execute(f'INSERT INTO {table}{col_text} VALUES {val_text}')
-        
-        word = item['term'] # Get the word for these entries
+	def process_item(self, item, spider):
+		
+		all_pos = ['adfix', 'adjective', 'adnoun', 'adverb', 'article', 'auxiliary verb', 'cardinal number', 'collective numeral',
+		   'conjunction', 'coverb', 'demonstrative determiner', 'demonstrative pronoun', 'determinative', 'determiner',
+		   'gerund', 'indefinite pronoun', 'infinitive', 'interjection', 'interrogative pronoun', 'intransitive verb',
+		   'noun', 'number', 'numeral', 'ordinal', 'ordinal number', 'part of speech', 'participle', 'particle',
+		   'personal pronoun', 'phrasal preposition', 'possessive adjective', 'possessive determiner', 'possessive pronoun',
+		   'postposition', 'preposition', 'preverb', 'pronoun', 'quasi-adjective', 'reciprocal pronoun', 'reflexive pronoun',
+		   'relative pronoun', 'speech disfluency', 'substantive', 'transitive', 'transitive verb', 'verb', 'verbal noun']
+		all_pos += ['infix', 'suffix', 'prefix', 'root']; # Needed for reconstruction
+		
+		def getNewKey(column, table):
+			try:
+				self.cursor.execute(f'SELECT max({column}) FROM {table}')
+			except Exception as e:
+				self.cursor.fetchall(); #Fetch to prevent this error from cascading
+				raise (e) # Raise so that no errors are introduced to the MYSQL Database
+			max_entry_id = self.cursor.fetchone()[0]; 
+			new_entry_id = max_entry_id + 1 if max_entry_id is not None else 0
+			return new_entry_id
+		
+		def insert(table, **kwargs):
+			columns = [str(k) for k,v in kwargs.items()]
+			col_text = '('+', '.join(columns)+')'
+			
+			values = [str(v) if type(v) != str else repr(v) for k,v in kwargs.items()]
+			val_text = '('+', '.join(values)+')'
+			
+			self.cursor.execute(f'INSERT INTO {table}{col_text} VALUES {val_text}')
+		
+		word = item['term'] # Get the word for these entries
+		
+		for key, entries in item.items():
+			if key in ('term', 'status'): continue
+			language = key
 
-        for key, entries in item.items():
-            if key == 'term': continue
-            language = key
+			try:
+				self.cursor.execute(
+					f'SELECT _id FROM etymologies e, languages l \
+					WHERE word = "{word}" and language_name = "{language}" and e.language_code = l.language_code')
+			except Exception as e:
+				self.cursor.fetchall(); #Fetch to prevent this error from cascading
+				raise (e) # Raise so that no errors are introduced to the MYSQL Database
+			ety_id_result = self.cursor.fetchone()
+			
+			if ety_id_result is not None: 
+				ety_id = ety_id_result[0]
+			else:
+				ety_id = getNewKey('_id', 'etymologies') + 1
+				self.cursor.execute(
+					f'INSERT INTO etymologies(_id, word, language_code) \
+						SELECT {ety_id}, "{word}", language_code FROM languages WHERE language_name = "{language}"')
+				
+			for entry in entries:
+				# Get a new entry key, make the entry connection
+				new_entry_id = getNewKey('entry_id', 'entry_connections')
+				insert('entry_connections', etymology_id=ety_id, entry_id=new_entry_id)
 
-            try:
-                self.cursor.execute(
-                    f'SELECT _id FROM etymologies e, languages l \
-                    WHERE word = "{word}" and language_name = "{language}" and e.language_code = l.language_code')
-            except Exception as e:
-                self.cursor.fetchall(); #Fetch to prevent this error from cascading
-                raise (e) # Raise so that no errors are introduced to the MYSQL Database
-            ety_id_result = self.cursor.fetchone()
-            
-            if ety_id_result is not None: 
-                ety_id = ety_id_result[0]
-            else:
-                ety_id = getNewKey('_id', 'etymologies') + 1
-                self.cursor.execute(
-                    f'INSERT INTO etymologies(_id, word, language_code) \
-                        SELECT {ety_id}, "{word}", language_code FROM languages WHERE language_name = "{language}"')
-                
-            for entry in entries:
-                # Get a new entry key, make the entry connection
-                new_entry_id = getNewKey('entry_id', 'entry_connections')
-                insert('entry_connections', etymology_id=ety_id, entry_id=new_entry_id)
+				for node_key, node_value in entry.items():
+					if node_key == 'pronunciation':
+						insert('entry_pronunciations', pronunciation=node_value, entry_id=new_entry_id)
 
-                for node_key, node_value in entry.items():
-                    if node_key == 'pronunciation':
-                        insert('entry_pronunciations', pronunciation=node_value, entry_id=new_entry_id)
+					elif node_key == 'etymology':
+						insert('entry_etymologies', etymology=node_value, entry_id=new_entry_id)
 
-                    elif node_key == 'etymology':
-                        insert('entry_etymologies', etymology=node_value, entry_id=new_entry_id)
+					elif node_key in all_pos:
+						new_pos_key = getNewKey('pos_id', 'entry_pos')
+						insert('entry_pos', pos_name=node_key, pos_id=new_pos_key, entry_id=new_entry_id)
 
-                    elif node_key in all_pos:
-                        new_pos_key = getNewKey('pos_id', 'entry_pos')
-                        insert('entry_pos', pos_name=node_key, pos_id=new_pos_key, entry_id=new_entry_id)
-
-                        for definition in node_value:
-                            insert('entry_definitions', definition=definition, pos_id=new_pos_key)
-#        self.conn.commit()
-        return item
+						for definition in node_value:
+							insert('entry_definitions', definition=definition, pos_id=new_pos_key)
+		return item
--- wiktionary_scraper/spiders/wiktionary_spider.py
+++ wiktionary_scraper/spiders/wiktionary_spider.py
@@ -1,120 +1,147 @@
 import scrapy, re, pandas as pd, mysql.connector, os
 from bs4 import BeautifulSoup
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 
 class WiktionarySpider(scrapy.Spider):
-    name = "wiktionary_spider"
-    
-    def start_requests(self):
-        user = os.environ['MYSQL_DB_USER']
-        password = os.environ['MYSQL_DB_PASSWORD']
-        host = '127.0.0.1'
-        database = 'etymology_explorer_staging'
-        conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
-        cursor = conn.cursor()
-        cursor.execute('SET NAMES utf8mb4;') #To avoid unicode issues
-        
-        # Option #2 for word-language pairs
-        cursor.execute('SELECT * FROM languages')
-        lc2ln = {row[1].decode(): row[0].decode() for row in cursor.fetchall()}; lc2ln['alu']
+	name = "wiktionary_spider"
+	
+	def start_requests(self):
+		user = os.environ['MYSQL_DB_USER']
+		password = os.environ['MYSQL_DB_PASSWORD']
+		host = '127.0.0.1'
+		database = 'etymology_explorer_staging'
+		conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
+		cursor = conn.cursor()
+		cursor.execute('SET NAMES utf8mb4;') #To avoid unicode issues
+		
+		cursor.execute('SELECT * FROM languages')
+		lc2ln = {row[1].decode(): row[0].decode() for row in cursor.fetchall()}; lc2ln['alu']
 
-        cursor.execute('SELECT word, language_code FROM etymologies WHERE _id NOT IN (SELECT DISTINCT etymology_id FROM entry_connections)')
-        new_terms = [[row[0].decode().strip(), row[1].decode()] for row in cursor.fetchall()]
+		cursor.execute('SELECT word, language_code FROM etymologies WHERE _id NOT IN (SELECT DISTINCT etymology_id FROM entry_connections)')
+		new_terms = [[row[0].decode().strip(), row[1].decode()] for row in cursor.fetchall()]
 
-        url_terms = [row[0] if not row[1].endswith('-pro') else 'Reconstruction%3A'+lc2ln[row[1]]+'%2F'+row[0] for row in new_terms]
-        
-        for term in set(url_terms):
-            url = 'https://en.wiktionary.org/api/rest_v1/page/html/' + term
-            term = re.sub('Reconstruction%3A.+?%2F(.*)', r'\1', term) #Remove reconstruction text if necessary
-            yield scrapy.Request(url=url, meta={'term':term}, callback=self.parse)
-        
-    def parse(self, response):
-        
-        # Must have this function within this context
-        def getDefsFromPOS(ety_pronunc_pos_node):
-            keep_def_tags = ('i', 'b', 'a', 'span', None)
-            node_data = []
-            if ety_pronunc_pos_node.parent.find('ol') is None: return None
-            for li in list(ety_pronunc_pos_node.parent.find('ol').children): # get defs from ordered list
-                if li.name != 'li': continue # This is a newline tag
+		url_terms = [row[0] if not row[1].endswith('-pro') else 'Reconstruction%3A'+lc2ln[row[1]]+'%2F'+row[0] for row in new_terms]
+		
+		for term in set(url_terms):
+			url = 'https://en.wiktionary.org/api/rest_v1/page/html/' + term
+			term = re.sub('Reconstruction%3A.+?%2F(.*)', r'\1', term) #Remove reconstruction text if necessary
+			yield scrapy.Request(url=url, meta={'term':term}, callback=self.parse, errback=self.errback)
+		
+	def parse(self, response):
+		
+		# Must have this function within this context
+		def getDefsFromPOS(ety_pronunc_pos_node):
+			keep_def_tags = ('i', 'b', 'a', 'span', None)
+			node_data = []
+			if ety_pronunc_pos_node.parent.find('ol') is None: return None
+			for li in list(ety_pronunc_pos_node.parent.find('ol').children): # get defs from ordered list
+				if li.name != 'li': continue # This is a newline tag
 
-                if li.find('ol'): #Ordered list means the sub items are the definition
-                    for sub_li in list(li.find('ol').children):
-                        if sub_li.name != 'li': continue # Skip newline tags
+				if li.find('ol'): #Ordered list means the sub items are the definition
+					for sub_li in list(li.find('ol').children):
+						if sub_li.name != 'li': continue # Skip newline tags
 
-                        for child in sub_li.children:
-                            if child.name not in keep_def_tags: child.clear() #Get rid of quotes and subitems
+						for child in sub_li.children:
+							if child.name not in keep_def_tags: child.clear() #Get rid of quotes and subitems
 
-                        node_data.append(sub_li.text.strip())
+						node_data.append(sub_li.text.strip())
 
-                else: # otherwise grab the text
-                    for child in li.children:
-                        if child.name not in keep_def_tags: child.clear() #Get rid of quotes and subitems
-                    node_data.append(li.text.strip())
-            return node_data
-        
-        #all_pos = response.meta['all_pos']
+				else: # otherwise grab the text
+					for child in li.children:
+						if child.name not in keep_def_tags: child.clear() #Get rid of quotes and subitems
+					node_data.append(li.text.strip())
+			return node_data
+		
+		all_pos = ['adfix', 'adjective', 'adnoun', 'adverb', 'article', 'auxiliary verb', 'cardinal number', 'collective numeral', 
+		   'conjunction', 'coverb', 'demonstrative determiner', 'demonstrative pronoun', 'determinative', 'determiner', 
+		   'gerund', 'indefinite pronoun', 'infinitive', 'interjection', 'interrogative pronoun', 'intransitive verb', 
+		   'noun', 'number', 'numeral', 'ordinal', 'ordinal number', 'part of speech', 'participle', 'particle', 
+		   'personal pronoun', 'phrasal preposition', 'possessive adjective', 'possessive determiner', 'possessive pronoun', 
+		   'postposition', 'preposition', 'preverb', 'pronoun', 'quasi-adjective', 'reciprocal pronoun', 'reflexive pronoun', 
+		   'relative pronoun', 'speech disfluency', 'substantive', 'transitive', 'transitive verb', 'verb', 'verbal noun']
+		all_pos += ['infix', 'suffix', 'prefix', 'root']; # Needed for reconstructions
+		
+		page_data = {'term': response.meta['term'], 'status': response.status} # Variable to store the data
+		# Check for 404 and save word in database. Save to wiktionary_page_dne if MYSQL is 404
 
-        all_pos = ['adfix', 'adjective', 'adnoun', 'adverb', 'article', 'auxiliary verb', 'cardinal number', 'collective numeral', 
-           'conjunction', 'coverb', 'demonstrative determiner', 'demonstrative pronoun', 'determinative', 'determiner', 
-           'gerund', 'indefinite pronoun', 'infinitive', 'interjection', 'interrogative pronoun', 'intransitive verb', 
-           'noun', 'number', 'numeral', 'ordinal', 'ordinal number', 'part of speech', 'participle', 'particle', 
-           'personal pronoun', 'phrasal preposition', 'possessive adjective', 'possessive determiner', 'possessive pronoun', 
-           'postposition', 'preposition', 'preverb', 'pronoun', 'quasi-adjective', 'reciprocal pronoun', 'reflexive pronoun', 
-           'relative pronoun', 'speech disfluency', 'substantive', 'transitive', 'transitive verb', 'verb', 'verbal noun']
-        
-        all_pos += ['infix', 'suffix', 'prefix', 'root']; # Needed for reconstructions
-        soup = BeautifulSoup(response.body, 'html.parser')
-        
-        #page = response.url.replace('https://en.wiktionary.org/api/rest_v1/page/html/', '')
-        #page = re.sub('Reconstruction:[^\/]+?\/(.*)', r'\1', page) #Remove reconstruction text if necessary
-        page_data = {'term': response.meta['term']} # Variable to store the data
+		soup = BeautifulSoup(response.body, 'html.parser')
+		
+		for lang_node in soup.find_all('h2'): # Go through each Language
+			language = lang_node.text.strip()
+			language_entries = [{}]
 
-        for lang_node in soup.find_all('h2'): # Go through each Language
-            language = lang_node.text.strip()
-            language_entries = [{}]
+			for ety_pronunc_pos_node in lang_node.parent.find_all('h3'): # Go through each ety,pronu, or pos
+				node_data = []
+				node_class = re.sub('_\d+| \d+', '', ety_pronunc_pos_node.text).lower() #removed '_x' info
 
-            for ety_pronunc_pos_node in lang_node.parent.find_all('h3'): # Go through each ety,pronu, or pos
-                node_data = []
-                node_class = re.sub('_\d+| \d+', '', ety_pronunc_pos_node.text).lower() #removed '_x' info
+				if node_class == 'etymology':
+					#Only looking at the first <p> element for etymology text
+					p_node = ety_pronunc_pos_node.parent.find('p')
+					if p_node is not None: 
+						entry_data = {'etymology': p_node.text.replace('\u200e', '')}
+					else:
+						continue
 
-                if node_class == 'etymology':
-                    #Only looking at the first <p> element for etymology text
-                    p_node = ety_pronunc_pos_node.parent.find('p')
-                    if p_node is not None: 
-                        entry_data = {'etymology': p_node.text}
-                    else:
-                        continue
+					for sub_ety_pos in ety_pronunc_pos_node.parent.find_all('h4'):
+						if sub_ety_pos.text.lower() in all_pos:
+							defs = getDefsFromPOS(sub_ety_pos)
+							if defs is None:
+								continue
+							else:
+								entry_data[sub_ety_pos.text.lower()] = defs
 
-                    for sub_ety_pos in ety_pronunc_pos_node.parent.find_all('h4'):
-                        if sub_ety_pos.text.lower() in all_pos:
-                            defs = getDefsFromPOS(sub_ety_pos)
-                            if defs is None:
-                                continue
-                            else:
-                                entry_data[sub_ety_pos.text.lower()] = defs
+					if any(['etymology' in entry for entry in language_entries]): #If an etymology already exists add to new entry
+						language_entries.append(entry_data)
+					else:
+						language_entries[0].update(entry_data)
 
-                    if any(['etymology' in entry for entry in language_entries]): #If an etymology already exists add to new entry
-                        language_entries.append(entry_data)
-                    else:
-                        language_entries[0].update(entry_data)
+				# Need to see if there are sub items of this etymology
+				elif node_class == 'pronunciation':
+					ipa_nodes = ety_pronunc_pos_node.parent.select('span.IPA') #dataquest.io/blog/web-scraping-tutorial-python/
+					if ipa_nodes: #Only add pronunciation if there are ipa_nodes
+						node_data = ipa_nodes[0].text
+						language_entries[0]['pronunciation'] = node_data
 
-                # Need to see if there are sub items of this etymology
-                elif node_class == 'pronunciation':
-                    ipa_nodes = ety_pronunc_pos_node.parent.select('span.IPA') #dataquest.io/blog/web-scraping-tutorial-python/
-                    if ipa_nodes: #Only add pronunciation if there are ipa_nodes
-                        node_data = ipa_nodes[0].text
-                        language_entries[0]['pronunciation'] = node_data
+				elif node_class in all_pos: # Here are the definitions
+					defs = getDefsFromPOS(ety_pronunc_pos_node)
+					if defs is None:
+						continue
+					else:
+						language_entries[0][node_class] = defs
 
-                elif node_class in all_pos: # Here are the definitions
-                    defs = getDefsFromPOS(ety_pronunc_pos_node)
-                    if defs is None:
-                        continue
-                    else:
-                        language_entries[0][node_class] = defs
+				else: # Skip all other node_classes
+					continue
 
-                else: # Skip all other node_classes
-                    continue
+				page_data[language] = language_entries #Add all the language entries to the language 
 
-                page_data[language] = language_entries #Add all the language entries to the language 
+		yield page_data
 
-        yield page_data
+	def errback(self, failure):
+		# log all failures
+		# self.logger.error(repr(failure))
+
+		# in case you want to do something special for some errors,
+		# you may need the failure's type:
+
+		if failure.check(HttpError):
+			# these exceptions come from HttpError spider middleware
+			# you can get the non-200 response
+			response = failure.value.response
+			if response.status != 404:
+				self.logger.error('HttpError on %s', response.url)
+
+		elif failure.check(DNSLookupError):
+			# this is the original request
+			request = failure.request
+			self.logger.error(repr(failure))
+			self.logger.error('DNSLookupError on %s', request.url)
+
+		elif failure.check(TimeoutError, TCPTimedOutError):
+			request = failure.request
+			self.logger.error(repr(failure))
+			self.logger.error('TimeoutError on %s', request.url)
+		else:
+			self.logger.error('Unknown Error!')
+			self.logger.error(repr(failure))
